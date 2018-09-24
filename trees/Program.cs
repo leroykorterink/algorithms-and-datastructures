512c4ce44723935e675a5bf6231098c0fc3f90cb
Leroy Korterink <lkorterink@outlook.com>
2018-09-25 00:02:43 +0200

    Initial commit

---
+++ trees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace trees
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
