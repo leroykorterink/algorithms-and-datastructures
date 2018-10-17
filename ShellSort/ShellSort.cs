434e9b65462461f554f94d74290bb67d694bd4db
Leroy Korterink <lkorterink@outlook.com>
2018-10-17 20:19:17 +0200

    Create ShellSort and add more test to check cases with a custom sequence

--- ShellSort/ShellSort.cs
+++ ShellSort/ShellSort.cs
@@ -4,9 +4,42 @@ namespace ShellSort
 {
     public class ShellSort<T> where T : IComparable
     {
-        public static T[] SortArray(T[] input)
+        /// <summary>
+        /// Shellsort (Gapsort) using a sequence suggested by Gonnet
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        public static T[] SortArray(T[] input, double sequence = 2.2)
         {
-            throw new NotImplementedException();
+            var initialSequence = input.Length / 2;
+
+            if (sequence > initialSequence)
+                throw new ArgumentException("`sequence` cannot be higher than the initial sequence (|input| / 2)");
+
+            for (
+                var gap = initialSequence;
+                gap > 0;
+                gap = gap == 2 ? 1 : (int) (gap / sequence)
+            )
+            {
+                for (var index = gap; index < input.Length; index++)
+                {
+                    var currentElement = input[index];
+                    var fromIndex = index;
+
+                    for (
+                        ;
+                        fromIndex >= gap && currentElement.CompareTo(input[fromIndex - gap]) < 0;
+                        fromIndex -= gap
+                    )
+                        input[fromIndex] = input[fromIndex - gap];
+
+                    input[fromIndex] = currentElement;
+                }
+            }
+
+            return input;
         }
     }
 }
--- ShellSortTests/ShellSortTests.cs
+++ ShellSortTests/ShellSortTests.cs
@@ -1,3 +1,4 @@
+using System;
 using ShellSort;
 using Xunit;
 
@@ -6,7 +7,7 @@ namespace ShellSortTests
     public class ShellSortTests
     {
         [Fact]
-        public void Should_SortIntegersWithShellSort()
+        public void Should_SortIntegers_With_ShellSort()
         {
             var input = new[] {8, 5, 9, 2, 6, 3};
             var sortedInput = new[] {2, 3, 5, 6, 8, 9};
@@ -17,7 +18,7 @@ namespace ShellSortTests
         }
 
         [Fact]
-        public void Should_SortCharactersWithShellSort()
+        public void Should_SortCharacters_With_ShellSort()
         {
             var input = new[] {'d', 'c', 'a', 'b', 'f'};
             var sortedInput = new[] {'a', 'b', 'c', 'd', 'f'};
@@ -25,6 +26,26 @@ namespace ShellSortTests
             var result = ShellSort<char>.SortArray(input);
 
             Assert.Equal(result, sortedInput);
+        }
+
+        [Fact]
+        public void Should_SortIntegers_With_ShellSort_With_GivenSequence()
+        {
+            var input = new[] {8, 5, 9, 2, 6, 3, 12, 13, 14, 15, 16, 17, 18};
+            var sortedInput = new[] {2, 3, 5, 6, 8, 9, 12, 13, 14, 15, 16, 17, 18};
+
+            var result = ShellSort<int>.SortArray(input, 4);
+
+            Assert.Equal(result, sortedInput);
+        }
+
+        [Fact]
+        public void Should_ThrowError_When_GivenSequenceIsTooHigh()
+        {
+            const int sequence = 4; // The sequence is too high because the initial sequence is 3. 7 / 2 = 3
+            var input = new[] {8, 5, 9, 2, 6, 3, 12}; // Input length 7
+
+            Assert.Throws<ArgumentException>(() => ShellSort<int>.SortArray(input, sequence));
         }
     }
 }
